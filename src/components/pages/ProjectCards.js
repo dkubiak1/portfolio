b2cf3a69853b52b02acdbd727833d449a6d4b50f
Daniel Kubiak <dan.kubiak@gmail.com>
2018-10-15 16:02:38 -0700

    fixed filepaths

--- src/components/pages/ProjectCards.js
+++ src/components/pages/ProjectCards.js
@@ -4,28 +4,28 @@ import CardModel from "../Card";
 
 const projects = [
     {
-        title: "Web-Scraper", image: "/webscraper.png", desc: "Scrapes BBC News using MongoDB.  \nAdd/delete notes", id: 0, link: "https://bbc-news-webscraper.herokuapp.com/" 
+        title: "Web-Scraper", image: "https://github.com/dkubiak1/portfolio/blob/master/public/webscraper.png?raw=true", desc: "Scrapes BBC News using MongoDB.  \nAdd/delete notes", id: 0, link: "https://bbc-news-webscraper.herokuapp.com/" 
     },
     {
-        title: "Ludum: a Phaser Game", image: "/ludum.png", desc: "Example of a simple dungeon-crawler using Phaser JS", id: 1, link: "https://ludum.herokuapp.com"
+        title: "Ludum: a Phaser Game", image: "https://github.com/dkubiak1/portfolio/blob/master/public/ludum.png?raw=true", desc: "Example of a simple dungeon-crawler using Phaser JS", id: 1, link: "https://ludum.herokuapp.com"
     },
     {
-        title: "BurgerApp", image: "/burgerapp.png", desc: "Demonstration of CRUD ops with SQL", id: 2, link: "https://immense-chamber-31608.herokuapp.com/"
+        title: "BurgerApp", image: "https://github.com/dkubiak1/portfolio/blob/master/public/burgerapp.png?raw=true", desc: "Demonstration of CRUD ops with SQL", id: 2, link: "https://immense-chamber-31608.herokuapp.com/"
     },
     {
-        title: "ClickGame", image: "/clickgame.png", desc: "Mnemonic click game using React", id: 3, link: "https://dkubiak1.github.io/ClickGame/"
+        title: "ClickGame", image: "https://github.com/dkubiak1/portfolio/blob/master/public/clickgame.png?raw=true", desc: "Mnemonic click game using React", id: 3, link: "https://dkubiak1.github.io/ClickGame/"
     },
     {
-        title: "FriendFinder", image: "/friendfinder.png", desc: "Basic algorithm to calculate and compare user inputs", id: 4, link: "https://sheltered-ravine-51533.herokuapp.com/"
+        title: "FriendFinder", image: "https://github.com/dkubiak1/portfolio/blob/master/public/friendfinder.png?raw=true", desc: "Basic algorithm to calculate and compare user inputs", id: 4, link: "https://sheltered-ravine-51533.herokuapp.com/"
     },
     {
-        title: "Trivia Game", image: "/triviagame.png", desc: "Web quiz with timer and SFX", id: 5, link: "https://dkubiak1.github.io/TriviaGame/"
+        title: "Trivia Game", image: "https://github.com/dkubiak1/portfolio/blob/master/public/triviagame.png?raw=true", desc: "Web quiz with timer and SFX", id: 5, link: "https://dkubiak1.github.io/TriviaGame/"
     },
     {
-        title: "VidWall", image: "/vidwall.png", desc: "YouTube video-magnet app using FireBase", id: 6, link: "https://dkubiak1.github.io/VidWall/"
+        title: "VidWall", image: "https://github.com/dkubiak1/portfolio/blob/master/public/vidwall.png?raw=true", desc: "YouTube video-magnet app using FireBase", id: 6, link: "https://dkubiak1.github.io/VidWall/"
     },
     {
-        title: "ImageDetection", image: "/facedetection.png", desc: "Use Google Vision API with your webcam!", id: 7, link: "https://face-detection6969.herokuapp.com/"
+        title: "ImageDetection", image: "https://github.com/dkubiak1/portfolio/blob/master/public/facedetection.png?raw=true", desc: "Use Google Vision API with your webcam!", id: 7, link: "https://face-detection6969.herokuapp.com/"
     },
     {
         title: "CLI-mazon", image: "https://github.com/dkubiak1/portfolio/blob/master/public/bamazon.png?raw=true", desc: "NodeJS CLI app simulates inventory using SQL", id: 8, link: "https://github.com/dkubiak1/bamazon"
