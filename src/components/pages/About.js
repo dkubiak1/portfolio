c2e854b4d028ab6425bc022fcdd15e2152bcd014
Daniel Kubiak <dan.kubiak@gmail.com>
2018-10-18 16:55:09 -0700

    updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -46,7 +46,7 @@ class About extends React.Component {
           <div>
           <Badge>Frameworks/Libraries:
           </Badge>
-       <p>React, DotNet, PixiJS, PhaserJS, JQuery and more!</p>
+       <p>ReactJS, .NET, PixiJS, PhaserJS, JQuery and more!</p>
         </div> 
         </div>     
 
